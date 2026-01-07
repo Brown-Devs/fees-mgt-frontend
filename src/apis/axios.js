bd83d7df6fd408ecaa0502229e2c61b47eb81ba2
Brown-Devs <storywiseclasses@gmail.com>
2026-01-07 22:45:35 +0530

    changes

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
+// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
