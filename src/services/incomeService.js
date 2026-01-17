8ec8b02e637dcd3166d6b2543433fc99fa81aa4e
Brown-Devs <storywiseclasses@gmail.com>
2026-01-17 18:10:59 +0530

    changes

--- src/services/incomeService.js
+++ src/services/incomeService.js
@@ -1,15 +1,17 @@
-import axios from "axios";
-const api = axios.create();
-const authHeader = () => ({ Authorization: `Bearer ${localStorage.getItem("token")}` });
+import api from "../apis/axios";
 
+// Fetch list
 export const fetchIncomes = (params = {}) =>
-  api.get("/api/incomes", { params, headers: authHeader() });
+  api.get("/api/incomes", { params });
 
+// Create
 export const createIncome = (payload) =>
-  api.post("/api/incomes", payload, { headers: authHeader() });
+  api.post("/api/incomes", payload);
 
+// Update
 export const updateIncome = (id, payload) =>
-  api.put(`/api/incomes/${id}`, payload, { headers: authHeader() });
+  api.put(`/api/incomes/${id}`, payload);
 
+// Delete
 export const deleteIncome = (id) =>
-  api.delete(`/api/incomes/${id}`, { headers: authHeader() });
+  api.delete(`/api/incomes/${id}`);
